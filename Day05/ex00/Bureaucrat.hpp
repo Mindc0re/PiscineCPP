67d832b5cd75bd41127049923f7c70f827670758
Simon GAUDIN <simongaudin65@gmail.com>
2018-10-08 12:26:04 +0200

    Day05 began, ex00 done

---
+++ Day05/ex00/Bureaucrat.hpp
@@ -0,0 +1,53 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class Bureaucrat
+{
+
+public:
+	Bureaucrat();
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(Bureaucrat const &inst);
+	~Bureaucrat();
+
+	Bureaucrat &		operator=(Bureaucrat const &rhs);
+
+	std::string			getName() const;
+	int 				getGrade() const;
+
+	void				incrementGrade();
+	void				decrementGrade();
+
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			GradeTooHighException();
+			GradeTooHighException(GradeTooHighException const &inst);
+			~GradeTooHighException() throw();
+			GradeTooHighException &	operator=(GradeTooHighException const &rhs);
+
+			const char *what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			GradeTooLowException();
+			GradeTooLowException(GradeTooLowException const &inst);
+			~GradeTooLowException() throw();
+			GradeTooLowException &	operator=(GradeTooLowException const &rhs);
+
+			const char *what() const throw();
+	};
+
+private:
+	const std::string	_name;
+	int 				_grade;
+
+};
+
+std::ostream &			operator<<(std::ostream &o, Bureaucrat const &rhs);
+
+#endif
---
+++ Day05/ex00/main.cpp
@@ -0,0 +1,62 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+
+	Bureaucrat stanley;
+	std::cout << stanley << std::endl;
+	stanley.incrementGrade();
+	std::cout << stanley << std::endl;
+	stanley.decrementGrade();
+	std::cout << stanley << std::endl;
+
+	Bureaucrat gandoulf("Gandoulf the Old", 1);
+	std::cout << gandoulf << std::endl;
+	gandoulf.decrementGrade();
+	std::cout << gandoulf << std::endl;
+	gandoulf.incrementGrade();
+	std::cout << gandoulf << std::endl;
+
+	
+	// Exception tooHigh on creation
+	try
+	{
+		Bureaucrat sauron("Sauron the Cheater", 0);
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	//Exception tooLow on creation
+	try
+	{
+		Bureaucrat sauron("Saroumane the Scum", 151);
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	//Exception tooHigh on incrementation
+	try
+	{
+		gandoulf.incrementGrade();
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	//Exception tooLow on decrementation
+	try
+	{
+		stanley.decrementGrade();
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	return 0;
+}
