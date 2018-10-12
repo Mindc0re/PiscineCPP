bf4324f6b8c3270d875e10d6f989b5185afdf994
Simon GAUDIN <simongaudin65@gmail.com>
2018-10-12 16:39:20 +0200

    ex01 done

--- Day08/ex01/main.cpp
+++ Day08/ex01/main.cpp
@@ -12,16 +12,23 @@ int main()
     {
         sp.addNumber(rand());
     }
+
+    Span sp2(sp);
     std::cout << "Added 30000 numbers" << std::endl;
     try 
     {
-        sp.addNumber(42);
+        sp2.addNumber(42);
     }
     catch (std::exception &e)
     {
         std::cout << "Could not add number because it exceeded the limits of the list" << std::endl;
     }
     
+    std::cout << "Spans of sp" << std::endl;
     std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl << std::endl;
+
+    std::cout << "Spans of sp2" << std::endl;
+    std::cout << sp2.shortestSpan() << std::endl;
+    std::cout << sp2.longestSpan() << std::endl;
 }
